fdc3aeb08e2412a0074226429f81fa4e4d3a2448
Yang-ByeongUn <143311964+Yang-ByeongUn@users.noreply.github.com>
2025-01-25 12:35:48 +0900

    [Medium] Kth Smallest Element in a BST
    [Medium] Kth Smallest Element in a BST

---
+++ LeetCode/byeongun/Kth Smallest Element in a BST.java
@@ -0,0 +1,37 @@
+package org.example;
+
+class TodayAlgorithm {
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    int count;
+    int val;
+
+    public int kthSmallest(TreeNode root, int k) {
+        count = k;
+        search(root);
+        return val;
+    }
+
+    private void search(TreeNode node) {
+        if (node == null) return;
+
+        search(node.left);
+
+        if (--count == 0) {
+            val = node.val;
+            return;
+        }
+
+        search(node.right);
+    }
+}
