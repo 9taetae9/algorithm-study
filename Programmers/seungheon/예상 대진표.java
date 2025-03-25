7f44edaa2b5197d09d34d73df9845dae9717fad9
FrogBaek <95288868+FrogBaek@users.noreply.github.com>
2025-03-25 11:03:00 +0900

    [Lv. 2] 예상 대진표

---
+++ Programmers/seungheon/예상 대진표.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int cnt = 0;
+        while (a != b) {
+            if (a % 2 != 0) a++;
+            if (b % 2 != 0) b++;
+            a /= 2;
+            b /= 2;
+            
+            cnt++;
+        }
+        
+        return cnt;
+    }
+}
